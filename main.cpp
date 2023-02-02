763cee7bb6f384263c4b0dd0a656b0d0f65f816a
Starinin Andrey <starinin-andrey@ya.ru>
2023-02-02 15:33:54 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -30,24 +30,40 @@ int main() {
 
     string line;
     while (getline(file, line)) {
-        int position = line.find(';', 0);
-        string last_name = line.substr(0, position);
-        line = line.substr(position + 1);
+        string temp_line = line;
 
-        position = line.find(';', 0);
-        string first_name = line.substr(0, position);
-        line = line.substr(position + 1);
+        int position = temp_line.find(';', 0);
+        string last_name = temp_line.substr(0, position);
+        temp_line = temp_line.substr(position + 1);
 
-        position = line.find(';', 0);
-        string patronymic = line.substr(0, position);
-        line = line.substr(position + 1);
+        position = temp_line.find(';', 0);
+        string first_name = temp_line.substr(0, position);
+        temp_line = temp_line.substr(position + 1);
 
-        position = line.find(';', 0);
-        string date_of_birth = line.substr(0, position);
+        position = temp_line.find(';', 0);
+        string patronymic = temp_line.substr(0, position);
+        temp_line = temp_line.substr(position + 1);
+
+        position = temp_line.find(';', 0);
+        string date_of_birth = temp_line.substr(0, position);
+
+        string temp_date = date_of_birth;
+        position = temp_date.find('.', 0);
+        int day = stoi(temp_date.substr(0, position));
+        temp_date = temp_date.substr(position + 1);
+
+        position = temp_date.find('.', 0);
+        int month = stoi(temp_date.substr(0, position));
+        temp_date = temp_date.substr(position + 1);
+
+        int year = stoi(temp_date.substr(0));
 
         cout << "Пользователь: ";
-        cout << last_name << " " << first_name << " " << patronymic << ", ";
-        cout << date_of_birth << endl;
+        cout << last_name << " " << first_name << " " << patronymic << endl;
+        cout << "Дата рождения: " << endl;
+        cout << "год: " << year << endl;
+        cout << "месяц: " << month << endl;
+        cout << "день: " << day << endl;
     }
 
     return 0;
